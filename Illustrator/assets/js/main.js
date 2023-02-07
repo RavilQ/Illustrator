4cac4e83b821d9527457a7f718f19ac6ccf4fad9
Ravil <ravil.quliyev.98@bk.ru>
2023-02-07 22:19:42 +0400

    Illustrator

--- Illustrator/assets/js/main.js
+++ Illustrator/assets/js/main.js
@@ -129,3 +129,19 @@ $(function () {
   );
 });
 
+
+
+
+$(function(){
+  $(".heading-compose").click(function() {
+    $(".side-two").css({
+      "left": "0"
+    });
+  });
+
+  $(".newMessage-back").click(function() {
+    $(".side-two").css({
+      "left": "-100%"
+    });
+  });
+}) 
